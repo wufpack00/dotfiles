cc2a665423df8ba67401c933d881aba39b30b451
Carey Rogers <crogers@sciquest.com>
2016-10-17 19:48:42 +0000

    use getops for specifying which files to use

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,70 +1,78 @@
 #!/bin/bash
-############################
+#-----------------------------
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+#-----------------------------
 
-########## Variables
-set -x;
+#set -x;
 
-files=".bash* .vim* .gitconfig"    	  # list of files/folders to symlink in homedir
+CORE_BASH=".bashrc .bash_aliases .bash_prompt .bash_completion .bash_functions .bash_profile"
+CYGWIN_BASH=".bash_cygwin"
+MACOS_BASH=".bash_macos"
+WORK_BASH=".bash_work"
+TMUX_CONFIG=".tmux*"
+VIM_CONFIG=".vim*"
+GIT_CONFIG=".gitconfig"
 
-while [ ! $# -eq 0 ]
+# default to only core bash, vim and git dot files
+CONFIG_FILES="$CORE_BASH $VIM_CONFIG $GIT_CONFIG"
+
+while getopts ":acmtwh" FLAG
 do
-    case "$1" in
-        --help | -h)
-            echo "Usage is ??"
-            exit
-            ;;
-        --include-all | -a)
+    case $FLAG in
+        a)
             echo "Including all config"
-            exit
+            CONFIG_FILES="$CONFIG_FILES $CYGWIN_BASH $MACOS_BASH $WORK_BASH $TMUX_CONFIG"
             ;;
-        --include-cygwin | -c)
+        c)
             echo "Including cygwin config"
+            CONFIG_FILES="$CONFIG_FILES $CYGWIN_BASH"
+            ;;
+        h)
+            echo "Usage is ?? (at best)"
             exit
             ;;
-        --include-mac | -m)
+        m)
             echo "Including macos config"
-            exit
+            CONFIG_FILES="$CONFIG_FILES $MACOS_BASH"
             ;;
-        --include-tmux | -t)
+        t)
             echo "Including tmux config...Press prefix + I (capital I, as in Install) to fetch the configured tpm plugins."
-            files="$files .tmux*"
-            exit
+            CONFIG_FILES="$CONFIG_FILES $TMUX_CONFIG"
             ;;
-        --include-work | -w)
+        w)
             echo "Including work config"
+            CONFIG_FILES="$CONFIG_FILES $WORK_BASH"
+            ;;
+        \?)
+            echo "Invalid option: $OPTARG"
             exit
             ;;
     esac
-    shift
 done
 
+#This tells getopts to move on to the next argument.
+shift $((OPTIND-1))  
 
-ts=$(date +"%Y%m%d%H%M%S")
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_$ts             # old dotfiles backup directory
+# remove any duplicate entries 
+CONFIG_FILES=$( echo "$CONFIG_FILES" | xargs -n1 | sort -u | xargs)
 
-##########
+CURRENT_TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+SOURCE_DIR=~/dotfiles                    
+BACKUP_DIR=~/dotfiles_$CURRENT_TIMESTAMP   
 
-# create dotfiles_old in homedir
-mkdir -p $olddir
+
+# create a backup directory for existing files
+mkdir -p $BACKUP_DIR
 
 # change to the dotfiles directory
-cd $dir
+cd $SOURCE_DIR
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-   # if [ -d "$file" ] ; then
-   #     echo "$file is a directory"
-   #     mv ~/$file $file_$ts
-   #     cp -R $dir/$file ~
-   # else
-        if [ -f "~/$file" ] ; then
-            mv ~/$file $olddir/
-        fi
-        ln -s $dir/$file ~/$file
-   # fi
+for file in $CONFIG_FILES; do
+   if [ -f "~/$file" ] ; then
+        mv ~/$file $BACKUP_DIR/
+   fi
+   ln -s $SOURCE_DIR/$file ~/$file
 done
 
 source ~/.bashrc
