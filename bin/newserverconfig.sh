f0e27a3e19df8b8030bbf3f325f3a9965737b129
Carey Rogers <crogers@sciquest.com>
2016-11-18 17:46:04 +0000

    get copy of git bash prompt scripts too

--- bin/newserverconfig.sh
+++ bin/newserverconfig.sh
@@ -13,3 +13,5 @@ ssh-copy-id $NEWSERVER
 
 rsync -avg --exclude="*cygwin" --exclude="*history" $HOME/.bash* $NEWSERVER:
 rsync -avg $HOME/.vimrc $NEWSERVER:
+
+ssh $NEWSERVER "curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >> ~/.git-completion.bash; curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh >> ~/.git-prompt.sh"
