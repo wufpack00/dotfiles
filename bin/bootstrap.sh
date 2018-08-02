484efc7404d43f33d80d8d0c2d1614a43c2319a0
Carey Rogers <Carey.Rogers@sas.com>
2018-08-02 15:29:48 -0400

    overwrite files retrieved via curl to ensure script can be run multiple times

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -101,7 +101,7 @@ for file in $CONFIG_FILES; do
     fi
 done
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >> ~/.git-completion.bash
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh >> ~/.git-prompt.sh
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 
 source $TARGET_DIR/.bashrc
