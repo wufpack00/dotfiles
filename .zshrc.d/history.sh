b1c9af717c2d5fa33d630a3595ff7c4cac01a216
Carey Rogers <wufpack00@gmail.com>
2018-10-29 13:51:17 -0400

    maintain history across sessions

--- .zshrc.d/history.sh
+++ .zshrc.d/history.sh
@@ -1,5 +1,15 @@
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
+HISTSIZE=50000
+SAVEHIST=10000
+
+## History command configuration
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt inc_append_history     # add commands to HISTFILE in order of execution
+setopt share_history          # share command history data
 
 HISTORY_IGNORE="([fb]g|ll|lo|h|cl|su|ls|cd|pwd|exit|cd ..)"
 
