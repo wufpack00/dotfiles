247307c91d43342b9d851e174b463894ddfc6563
Carey Rogers <carey@Careys-MBP.lan1>
2022-07-03 17:08:35 -0400

    feat: start minimal config

--- .bashrc.d/ssh-completion.sh
+++
@@ -1,7 +0,0 @@
-# ssh-completion.sh 
-
-# ssh auto-complete from known_hosts file
-if [ -f "${HOME}/.ssh/known_hosts" ]; then
-    complete -W "$(echo `cat ${HOME}/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
-fi
-
