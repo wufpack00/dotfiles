650ddafc9271ebcb98e3e2bc570e3aebf1186831
Carey Rogers <carey@Careys-MBP.lan1>
2022-07-04 00:11:34 -0400

    fix: add aliases

--- .rc.d/vars-aws.sh
+++
@@ -1,4 +0,0 @@
-# set variables for default profile
-if command_exists ruby ; then
-    $($HOME/.dotfiles/bin/aws-switch.rb)
-fi
--- .zshrc.d/aliases.sh
+++ .zshrc.d/aliases.sh
@@ -2,3 +2,8 @@ alias -g G='| grep -i'
 
 alias cat='bat'
 alias d='dirs -v | head -10'
+alias grep='grep --color=always'
+alias du='du -h'
+alias df='df -h'
+alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
+alias ll='ls -FlAhp --color=always'
